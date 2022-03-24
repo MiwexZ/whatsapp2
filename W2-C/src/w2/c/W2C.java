6c2671b7f31c03942fb6d5326ce133951e01bfd6
MiwexZ <miguelro9@icloud.com>
2022-03-24 13:58:25 +0100

    Berenjena

--- W2-C/src/w2/c/W2C.java
+++ W2-C/src/w2/c/W2C.java
@@ -6,6 +6,7 @@ public class W2C {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Adios world");
+        System.out.println("Berenjena");
     }
     
 }
