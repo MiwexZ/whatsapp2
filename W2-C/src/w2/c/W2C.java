80e4056a43b3e66f18556443f651aa41b981a1a2
miguelmmm <manzano3.infor@gmail.com>
2022-04-04 13:52:44 +0200

    JUAN ROPERO2

--- W2-C/src/w2/c/W2C.java
+++ W2-C/src/w2/c/W2C.java
@@ -9,7 +9,7 @@ public class W2C {
         System.out.println("Berenjena");
         System.out.println("Segundo dia");
         System.out.println("Esto es desde linu");
-
+	System.out.println("JUAN ROPERO TIENDE LA ROPA EN EL ROPERO DEL RAPERO");
         
     }
     
